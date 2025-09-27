ac8c78533931fdfb9c686057b898c0623c35f546
DevByAli <aligrewal21@gmail.com>
2025-09-27 13:58:34 +0500

    Learn how to navigate from one page to another in admin panel. Learn how to compute the field.

--- store/admin.py
+++ store/admin.py
@@ -1,41 +1,47 @@
 from django.contrib import admin
 from . import models
 from django.db.models import Count
+from django.utils.html import format_html
+from django.urls import reverse
+from urllib.parse import urlencode
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
-    list_editable = ['unit_price']
-    list_select_related = ['collection']
+    list_editable = ["unit_price"]
+    list_select_related = ["collection"]
     list_per_page = 10
-    
-    
+
+    @admin.display(ordering="collection__title")
     def collection_title(self, product):
         return product.collection.title
 
-    @admin.display(ordering='inventory')
+    @admin.display(ordering="inventory")
     def inventory_status(self, product):
         if product.inventory < 10:
-            return 'Low'
-        return 'Ok'
-    
+            return "Low"
+        return "Ok"
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'product_count']
+    list_display = ["title", "product_count"]
 
-
-    @admin.display(ordering='product_count')
+    @admin.display(ordering="product_count")
     def product_count(self, collection):
-        return collection.product_count
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).annotate(
-            product_count=Count('product')
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": str(collection.id)})
         )
         
-    
+        return format_html('<a href="{}">{}</a>', url, collection.product_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(product_count=Count("product"))
+
+
 @admin.register(models.Promotion)
 class PromotionAdmin(admin.ModelAdmin):
     list_display = ["description", "discount"]
@@ -44,9 +50,9 @@ class PromotionAdmin(admin.ModelAdmin):
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "email", "membership"]
-    list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
-    
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+
 
 @admin.register(models.Address)
 class AddressAdmin(admin.ModelAdmin):
@@ -61,8 +67,8 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(models.OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ["order", "product", "quantiy", "unit_price"]
-    
-    
+
+
 # Register your models here.
 # admin.site.register(models.Product, ProductAdmin)
 # admin.site.register(models.Collection, CollectionAdmin)

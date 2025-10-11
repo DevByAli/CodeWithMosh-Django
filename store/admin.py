7d01f1e9a7304f4b496aea4400f2b9ec259b5bf0
DevByAli <aligrewal21@gmail.com>
2025-10-11 13:08:11 +0500

    Implement navigation from the Customer Page -> Orders Page in admin

--- store/admin.py
+++ store/admin.py
@@ -49,9 +49,24 @@ class PromotionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "email", "membership"]
+    list_display = ["first_name", "last_name", "email", "membership", "orders"]
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
+    
+    @admin.display(ordering='orders')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({"customer__id": str(customer.id)})
+        )
+        
+        return format_html('<a href={}>{}</a>',url, customer.orders)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders=Count('order')
+        )
 
 
 @admin.register(models.Address)

656b92cae0239674978a6e84da7101c63ff85cf0
DevByAli <aligrewal21@gmail.com>
2025-10-18 12:10:03 +0500

    Learn to create the custom field in searilizers.

--- store/serializers.py
+++ store/serializers.py
@@ -6,3 +6,7 @@ class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    unit_price_with_tax = serializers.SerializerMethodField(method_name='get_unit_price_with_tax') # method_name is required when method_name != "get_{field_name}"
+    
+    def get_unit_price_with_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)

3d7b15232e9dc23abf373b7971f9976fdd9f2250
DevByAli <aligrewal21@gmail.com>
2025-12-20 22:47:06 +0500

    Add the DJOSER jwt authentication

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     "debug_toolbar",
     'playground',
     'store',
@@ -148,12 +149,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     
     # You can define the GLOBAL pagination for each modal here
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
     # 'PAGE_SIZE_QUERY_PARAM': 'page_size'
+}
+
+SIMPLE_JWT = {
+    # Here JWT is the prefix of token like Bearer
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 # This is now the AUTH USER Model going to use.

a879726c92c2810c0025314dffd4180b6a95078a
DevByAli <aligrewal21@gmail.com>
2026-01-03 23:17:14 +0500

    Improve the Order creation. Send the order object when order is created and add the validation of empty cart while creating the order.

--- store/serializers.py
+++ store/serializers.py
@@ -145,23 +145,23 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
     
     
-    def validate_cart_id(self, value):
-        if not Cart.objects.filter(pk=value).exists():
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
             raise Http404("Cart not found.")
-        return value
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise Http404("No item found in cart.")     
+        return cart_id
     
     def save(self, **kwargs):
         with transaction.atomic():
             cart_id = self.validated_data['cart_id']
             user_id = self.context.get('user_id')
             
-            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
-            if cart_items.count() == 0:
-                raise Http404("No item found in cart.")
             
             (customer, created) = Customer.objects.get_or_create(user_id=user_id)
             order = Order.objects.create(customer=customer)
             
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
             order_items = [
                 OrderItem(
                     order=order,
@@ -174,3 +174,5 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.filter(pk=cart_id).delete()
+            
+            return order

a9a0851d5a99d4c520fa535a997a90b63f760135
DevByAli <aligrewal21@gmail.com>
2026-03-07 15:07:41 -0800

    Run continous Test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -18,6 +18,9 @@ Run specific Test:
 - pytest store/tests/test_collections.py::TestCreateCollection::test_if_user_is_anonymous_returns_401
 - pytest -f anonymous # Test include anonymous name
         
+
+Run Conitnous Testing via Pytest-Watcher
+- ptw
 """
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -26,7 +29,7 @@ import pytest
 # Always start name with Test, otherwise pytest will recognize it.
 @pytest.mark.django_db
 class TestCreateCollection:
-    @pytest.mark.skip # Skip test
+    # @pytest.mark.skip # Skip test
     def test_if_user_is_anonymous_returns_401(self):
 
         # Act
